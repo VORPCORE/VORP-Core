c163215c5a86702e7e74b4a89b33909dd6dbc2ea
local9 <apjcookie1@hotmail.com>
2022-01-25 12:01:09 +0000

    Initial class setup

--- Vorp.Core.Server/PluginManager.cs
+++ Vorp.Core.Server/PluginManager.cs
@@ -1,9 +1,17 @@
-﻿using System;
+﻿using CitizenFX.Core;
+using System;
 
 namespace Vorp.Core.Server
 {
-    public class PluginManager
+    public class PluginManager : BaseScript
     {
+        public static PluginManager Instance { get; private set; }
+        public static PlayerList PlayerList { get; private set; }
 
+        public PluginManager()
+        {
+            PlayerList = Players;
+            Instance = this;
+        }
     }
 }
