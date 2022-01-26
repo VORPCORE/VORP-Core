4931fe12f28b932e6db26c61a31e84ef11d6d62a
local9 <apjcookie1@hotmail.com>
2022-01-26 17:09:37 +0000

    initial class files

---
+++ Vorp.Shared/Models/Character.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vorp.Shared.Models
+{
+    public class Character
+    {
+    }
+}
---
+++ Vorp.Shared/Models/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vorp.Shared.Models
+{
+    public class User
+    {
+        public string ServerId { get; private set; }
+
+        // DB Keys
+        public string SteamIdentifier { get; private set; }
+        public string LicenseIdentifier { get; private set; }
+
+        // Administration
+        public string Group { get; private set; }
+        public int Warnings { get; private set; }
+
+        // Character Items
+        public Dictionary<int, Character> Characters { get; private set; } = new Dictionary<int, Character>();
+        public int NumberOfCharacters => Characters.Count;
+
+        public User(string serverId,
+                    string steamId,
+                    string license,
+                    string group,
+                    int warnings)
+        {
+            ServerId = serverId;
+            SteamIdentifier = steamId;
+            LicenseIdentifier = license;
+            Group = group;
+            Warnings = warnings;
+        }
+
+        // Methods
+    }
+}
