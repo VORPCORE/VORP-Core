9831209f7ba066582884500e5635aac694536833
local9 <apjcookie1@hotmail.com>
2022-01-26 17:10:38 +0000

    Remove console colors

--- vorpcore_sv/Utils/LogManager.cs
+++ vorpcore_sv/Utils/LogManager.cs
@@ -13,30 +13,7 @@ namespace vorpcore_sv.Utils
 
         public static void WriteLog(string msg, string type)
         {
-            switch (type)
-            {
-                case "warning": // Warning Yellow
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.WriteLine(msg);
-                    break;
-                case "error": // Error Red
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine(msg);
-                    break;
-                case "success": // Success Green
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine(msg);
-                    break;
-                case "info": // Info Blue Cyan
-                    Console.ForegroundColor = ConsoleColor.Cyan;
-                    Console.WriteLine(msg);
-                    break;
-                default: // Normal
-                    Console.ForegroundColor = ConsoleColor.White;
-                    Console.WriteLine(msg);
-                    break;
-            }
-            Console.ForegroundColor = ConsoleColor.White;
+            Debug.WriteLine($"[{type}] {msg}");
         }
 
     }
