1c6190f4ae201370d47b561b79a948c6aec19b93
local9 <apjcookie1@hotmail.com>
2022-01-27 11:09:32 +0000

    Added Dapper database methods

---
+++ Vorp.Core.Server/Database/DapperDatabase.cs
@@ -0,0 +1,88 @@
+﻿using Dapper;
+using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace Vorp.Core.Server.Database
+{
+    internal class DapperDatabase<T>
+    {
+        private static string _connectionString => GetConvar("mysql_connection_string", "missing");
+
+        public static List<T> GetList(string query, DynamicParameters args = null)
+        {
+            var watch = Stopwatch.StartNew();
+            try
+            {
+                using (var conn = new MySqlConnection(_connectionString))
+                {
+                    return conn.Query<T>(query, args).AsList();
+                }
+            }
+            catch (Exception ex)
+            {
+                SqlExceptionHandler(query, ex.Message, watch.ElapsedMilliseconds);
+            }
+            finally
+            {
+                watch.Stop();
+            }
+            return null;
+        }
+
+        public static T GetSingle(string query, DynamicParameters args = null)
+        {
+            var watch = Stopwatch.StartNew();
+            try
+            {
+                using (var conn = new MySqlConnection(_connectionString))
+                {
+                    return conn.Query<T>(query, args).FirstOrDefault();
+                }
+            }
+            catch (Exception ex)
+            {
+                SqlExceptionHandler(query, ex.Message, watch.ElapsedMilliseconds);
+            }
+            finally
+            {
+                watch.Stop();
+            }
+            return default(T);
+        }
+
+        public static bool Execute(string query, DynamicParameters args = null)
+        {
+            var watch = Stopwatch.StartNew();
+            try
+            {
+                using (var conn = new MySqlConnection(_connectionString))
+                {
+                    return conn.Execute(query, args) > 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                SqlExceptionHandler(query, ex.Message, watch.ElapsedMilliseconds);
+            }
+            finally
+            {
+                watch.Stop();
+            }
+            return false;
+        }
+
+        private static void SqlExceptionHandler(string query, string exceptionMessage, long elapsedMilliseconds)
+        {
+            StringBuilder sb = new();
+            sb.Append("** SQL Exception **\n");
+            sb.Append($"Query: {query}\n");
+            sb.Append($"Exception Message: {exceptionMessage}\n");
+            sb.Append($"Time Elapsed: {elapsedMilliseconds}");
+            Logger.Error($"{sb}");
+        }
+    }
+}
