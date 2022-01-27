b1cba38c515661fa9409fd5af990905dd9892038
local9 <apjcookie1@hotmail.com>
2022-01-27 12:05:20 +0000

    fleshing out User more

--- Vorp.Shared/Records/User.cs
+++ Vorp.Shared/Records/User.cs
@@ -1,20 +1,34 @@
-﻿using System.Collections.Generic;
+﻿using Dapper;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using Vorp.Core.Server.Database;
 
 namespace Vorp.Shared.Records
 {
-    public class User
+    public record User
     {
-        public string ServerId { get; private set; }
+        #region Fields
 
+        [Description("identifier")]
+        public string SteamIdentifier { get; private set; }
+
+        [Description("group")]
+        public string Group { get; private set; }
+
+        [Description("warnings")]
+        public int Warnings { get; private set; }
+
+        [Description("banned")]
+        public bool Banned { get; private set; }
+
+        #endregion
+
+
+        public string ServerId { get; private set; }
         // DB Keys
         public ulong DiscordIdentifier { get; private set; }
-        public string SteamIdentifier { get; private set; }
         public string LicenseIdentifier { get; private set; }
-
-        // Administration
-        public string Group { get; private set; }
-        public int Warnings { get; private set; }
 
         // Character Items
         public Dictionary<int, Character> Characters { get; private set; } = new Dictionary<int, Character>();
@@ -47,7 +61,31 @@ namespace Vorp.Shared.Records
         // should review group to become a bit array of permissions?
         public void SetGroup(string group)
         {
-            Group = group;
+            DynamicParameters dynamicParameters = new DynamicParameters();
+            dynamicParameters.Add("id", SteamIdentifier);
+            dynamicParameters.Add("group", group);
+            bool changePersisted = DapperDatabase<User>.Execute("update users set `group` = @group where `identifier` = @id;", dynamicParameters);
+            if (changePersisted)
+                Group = group;
+        }
+
+        public void SetWarning(int warnings)
+        {
+            DynamicParameters dynamicParameters = new DynamicParameters();
+            dynamicParameters.Add("id", SteamIdentifier);
+            dynamicParameters.Add("warningCount", Warnings);
+            bool changePersisted = DapperDatabase<User>.Execute("update users set `warnings` = @warningCount where `identifier` = @id;", dynamicParameters);
+            if (changePersisted)
+                Warnings = warnings;
+        }
+
+        public void GetCharacters()
+        {
+            DynamicParameters dynamicParameters = new DynamicParameters();
+            dynamicParameters.Add("id", SteamIdentifier);
+            List<Character> characters = DapperDatabase<Character>.GetList("select * from characters where `identifier` = @id", dynamicParameters);
+            foreach (Character character in characters)
+                Characters.Add(character.CharacterId, character);
         }
 #endif
     }
