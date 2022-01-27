27925d248a08a8b2d59cd7fc5c3743ccc8257113
local9 <apjcookie1@hotmail.com>
2022-01-27 10:32:43 +0000

    Move diagnostics to shared project

---
+++ Vorp.Shared/Diagnostics/Logger.cs
@@ -0,0 +1,15 @@
+﻿namespace Vorp.Diagnostics
+{
+    internal class Logger
+    {
+        public static void Debug(string msg)
+        {
+            WriteLogMessage($"[DEBUG] {msg}");
+        }
+
+        private static void WriteLogMessage(string msg)
+        {
+            CitizenFX.Core.Debug.WriteLine(msg);
+        }
+    }
+}
