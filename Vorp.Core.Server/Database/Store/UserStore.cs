dd7488e2da91964083e21c381174f3a3f49b8c08
local9 <apjcookie1@hotmail.com>
2022-02-02 22:02:38 +0000

    Added method IsUserBanned

---
+++ Vorp.Core.Server/Database/Store/UserStore.cs
@@ -0,0 +1,23 @@
+﻿using Dapper;
+using System.Threading.Tasks;
+
+namespace Vorp.Core.Server.Database.Store
+{
+    internal static class UserStore
+    {
+        public static async Task<bool> IsUserInWhitelist(string steamIdent)
+        {
+            DynamicParameters dynamicParameters = new DynamicParameters();
+            dynamicParameters.Add("steam", steamIdent);
+            return await DapperDatabase<bool>.GetSingleAsync("SELECT TRUE FROM whitelist WHERE `identifier` = @steam LIMIT 1;", dynamicParameters);
+        }
+
+        // still use this, though txAdmin does it any way
+        public static async Task<bool> IsUserBanned(string steamIdent)
+        {
+            DynamicParameters dynamicParameters = new DynamicParameters();
+            dynamicParameters.Add("steam", steamIdent);
+            return await DapperDatabase<bool>.GetSingleAsync("SELECT TRUE FROM users WHERE `identifier` = @steam and `banned` = 1 LIMIT 1;", dynamicParameters);
+        }
+    }
+}
