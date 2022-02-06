c2f74b23131dd37370ad56fb2df240046325b570
local9 <apjcookie1@hotmail.com>
2022-02-06 15:15:23 +0000

    Added world player manager

---
+++ Vorp.Core.Client/Managers/GameWorld/WorldPlayerManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vorp.Core.Client.Environment.Entities;
+
+namespace Vorp.Core.Client.Managers.GameWorld
+{
+    public class WorldPlayerManager : Manager<WorldPlayerManager>
+    {
+        ClientConfigManager ClientConfigManager => ClientConfigManager.GetModule();
+        Dictionary<int, VorpPlayer> playersInScope = new Dictionary<int, VorpPlayer>();
+        float ConfigDistance => ClientConfigManager.PlayerNameConfig.Distance;
+
+        public override void Begin()
+        {
+
+        }
+
+        [TickHandler(SessionWait = true)]
+        private async Task OnWorldPlayer()
+        {
+            for (int activePlayerId = 0; activePlayerId < GetActivePlayers(); activePlayerId++)
+            {
+                if (playersInScope.ContainsKey(activePlayerId)) continue;
+                if (GetPlayerPed(activePlayerId) == PlayerPedId()) continue;
+
+                if (NetworkIsPlayerActive(activePlayerId))
+                {
+                    Vector3 playerCoords = GetEntityCoords(PlayerPedId(), false, false);
+                    Vector3 targetCoords = GetEntityCoords(activePlayerId, false, false);
+                    if (VorpAPI.Distance(playerCoords, targetCoords) < ConfigDistance)
+                    {
+                        int playerPedId = GetPlayerPed(activePlayerId);
+                        playersInScope[activePlayerId] = new VorpPlayer(activePlayerId, playerPedId);
+                    }
+                    else
+                    {
+                        DisposePlayer(activePlayerId);
+                    }
+                }
+                else
+                {
+                    DisposePlayer(activePlayerId);
+                }
+            }
+        }
+
+        void DisposePlayer(int activePlayerId)
+        {
+            if (playersInScope.ContainsKey(activePlayerId))
+                playersInScope[activePlayerId].Dispose();
+        }
+    }
+}
