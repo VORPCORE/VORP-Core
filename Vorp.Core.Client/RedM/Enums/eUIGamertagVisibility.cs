321956c347c7343cec5d251b6c3b681256afd54f
local9 <apjcookie1@hotmail.com>
2022-02-06 15:15:12 +0000

    initial work on internal VorpAPI till RedM makes one

---
+++ Vorp.Core.Client/RedM/Enums/eUIGamertagVisibility.cs
@@ -0,0 +1,10 @@
+﻿namespace Vorp.Core.Client.RedM.Enums
+{
+    internal enum eUIGamertagVisibility
+    {
+        UIGAMERTAGVISIBILITY_NONE,
+        UIGAMERTAGVISIBILITY_ICON,
+        UIGAMERTAGVISIBILITY_SIMPLE,
+        UIGAMERTAGVISIBILITY_COMPLEX
+    }
+}
---
+++ Vorp.Core.Client/RedM/VorpAPI.cs
@@ -0,0 +1,22 @@
+﻿using Vorp.Core.Client.RedM.Enums;
+
+namespace Vorp.Core.Client.RedM
+{
+    internal class VorpAPI
+    {
+        public static void SetMpGamerTagVisibility(int gamerTagId, eUIGamertagVisibility visibility)
+        {
+            Function.Call((Hash)0x93171DDDAB274EB8, gamerTagId, (int)visibility);
+        }
+
+        public static int CreateGamerTag(int playerId, string name, bool pointedTag = false, bool isRockstarClan = false, string clanTag = "", int clanFlag = 0)
+        {
+            return Function.Call<int>((Hash)0xD877AF112AD2B41B, playerId, name, pointedTag, isRockstarClan, clanTag, clanFlag);
+        }
+
+        public static float Distance(Vector3 start, Vector3 end)
+        {
+            return Vdist(start.X, start.Y, start.Z, end.X, end.Y, end.Z);
+        }
+    }
+}
