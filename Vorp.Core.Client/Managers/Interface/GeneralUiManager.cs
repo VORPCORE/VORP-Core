a9c8b0d9dd069d47a495e14f3e1b7f23f2c44c1c
local9 <apjcookie1@hotmail.com>
2022-02-06 23:04:37 +0000

    General UI Manager

---
+++ Vorp.Core.Client/Managers/Interface/GeneralUiManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Vorp.Core.Client.RedM.Enums;
+
+namespace Vorp.Core.Client.Managers.Interface
+{
+    public class GeneralUiManager : Manager<GeneralUiManager>
+    {
+        public override void Begin()
+        {
+            
+        }
+
+        [TickHandler(SessionWait = true)]
+        private async Task OnDisableHud()
+        {
+            API.DisableControlAction(0, (uint)eControls.HudSpecial, true);
+            API.DisableControlAction(0, (uint)eControls.RevealHud, true);
+        }
+    }
+}
