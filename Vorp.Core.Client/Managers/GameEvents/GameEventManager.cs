7d8eb1dce789896bf13c2c3cafb2febdec1de1c0
local9 <apjcookie1@hotmail.com>
2022-02-06 15:32:15 +0000

    Added Game Event Manager

---
+++ Vorp.Core.Client/Managers/GameEvents/GameEventManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vorp.Core.Client.Managers.GameEvents
+{
+    public class GameEventManager : Manager<GameEventManager>
+    {
+        public override void Begin()
+        {
+            EventHandler.Add("gameEventTriggered", new Action<string, List<dynamic>>(OnGameEventTriggered));
+        }
+
+        void OnGameEventTriggered(string name, List<dynamic> args)
+        {
+            Logger.Info($"game event '{name}' ({String.Join(", ", args.ToArray())})");
+        }
+    }
+}
