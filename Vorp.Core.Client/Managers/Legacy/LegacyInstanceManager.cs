58ef295d830c21cac4bdf696d97f91f865d1b6d8
local9 <apjcookie1@hotmail.com>
2022-02-06 15:27:39 +0000

    added LegacyInstanceManager

---
+++ Vorp.Core.Client/Managers/Legacy/LegacyInstanceManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Vorp.Core.Client.Managers.Legacy
+{
+    public class LegacyInstanceManager : Manager<LegacyInstanceManager>
+    {
+        public override void Begin()
+        {
+            EventHandler.Add("vorp:setInstancePlayer", new Action<bool>(OnSetPlayerInstance));
+        }
+
+        void OnSetPlayerInstance(bool instance)
+        {
+
+            switch (instance)
+            {
+                case true:
+                    NetworkStartSoloTutorialSession();
+                    break;
+                case false:
+                    NetworkEndTutorialSession();
+                    break;
+            }
+        }
+    }
+}
