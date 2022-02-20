c0cc28f8eab577420e6255c3e6498fb98331f817
local9 <apjcookie1@hotmail.com>
2022-02-20 17:03:22 +0000

    Handle data from the server when received for characters

--- Vorp.Core.Client/Managers/CharacterManagement/CharacterManager.cs
+++ Vorp.Core.Client/Managers/CharacterManagement/CharacterManager.cs
@@ -1,10 +1,15 @@
-﻿namespace Vorp.Core.Client.Managers.CharacterManagement
+﻿using Vorp.Shared.Records;
+
+namespace Vorp.Core.Client.Managers.CharacterManagement
 {
     public class CharacterManager : Manager<CharacterManager>
     {
         public override void Begin()
         {
-            
+            ClientGateway.Mount("vorp:character:list", new Action<Dictionary<int, Character>>(characters =>
+            {
+                Logger.Debug($"Received {characters.Count} characters from the server.");
+            }));
         }
     }
 }
