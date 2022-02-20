4779e5b7509f893336574c8f832fc7fc3962889b
local9 <apjcookie1@hotmail.com>
2022-02-20 15:48:10 +0000

    Added new managers for User, Character, and Inventory

---
+++ Vorp.Core.Client/Managers/CharacterManagement/CharacterManager.cs
@@ -0,0 +1,10 @@
+﻿namespace Vorp.Core.Client.Managers.CharacterManagement
+{
+    public class CharacterManager : Manager<CharacterManager>
+    {
+        public override void Begin()
+        {
+            
+        }
+    }
+}
