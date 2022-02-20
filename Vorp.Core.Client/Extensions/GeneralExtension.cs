7933260ae095e8925454e5912a3c29865507a71a
local9 <apjcookie1@hotmail.com>
2022-02-20 17:02:23 +0000

    Added extension for handling object to Json and back

---
+++ Vorp.Core.Client/Extensions/GeneralExtension.cs
@@ -0,0 +1,15 @@
+﻿namespace Vorp.Core.Client.Extensions
+{
+    static class GeneralExtension
+    {
+        public static string ToJson(this object obj)
+        {
+            return JsonConvert.SerializeObject(obj);
+        }
+
+        public static T FromJson<T>(this string str)
+        {
+            return JsonConvert.DeserializeObject<T>(str);
+        }
+    }
+}
