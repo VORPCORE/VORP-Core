9a3336941c5bf645ce3f195491ab16aaff55bd68
local9 <apjcookie1@hotmail.com>
2022-02-21 21:32:10 +0000

    Update events logger

--- Vorp.Shared/Diagnostics/EventLogger.cs
+++ Vorp.Shared/Diagnostics/EventLogger.cs
@@ -4,19 +4,35 @@ namespace Vorp.Diagnostics
 {
     public class EventLogger : IEventLogger
     {
+
+#if CLIENT
+        static string _loggingLevel = GetConvar2("vorp_logging_level", "none");
+#elif SERVER
+        static string _loggingLevel = GetConvar("vorp_logging_level", "none");
+#endif
+        static bool ShowOutput(string level)
+        {
+            string lowercase = _loggingLevel.ToLower();
+            if (lowercase == "all") return true;
+            return (lowercase == _loggingLevel);
+        }
+
         public void Debug(params object[] values)
         {
-            CitizenFX.Core.Debug.WriteLine(Format(values));
+            if (ShowOutput("debug"))
+                CitizenFX.Core.Debug.WriteLine(Format(values));
         }
 
         public void Info(params object[] values)
         {
-            CitizenFX.Core.Debug.WriteLine(Format(values));
+            if (ShowOutput("info"))
+                CitizenFX.Core.Debug.WriteLine(Format(values));
         }
 
         public void Error(params object[] values)
         {
-            CitizenFX.Core.Debug.WriteLine(Format(values));
+            if (ShowOutput("error"))
+                CitizenFX.Core.Debug.WriteLine(Format(values));
         }
 
         public string Format(object[] values)
