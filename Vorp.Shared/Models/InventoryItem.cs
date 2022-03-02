ee2fb272a54bb055658fc9053ecc0a82d27b4fef
local9 <apjcookie1@hotmail.com>
2022-03-02 20:37:32 +0000

    Added InventoryItem

---
+++ Vorp.Shared/Models/InventoryItem.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.Serialization;
+
+namespace Vorp.Shared.Models
+{
+    [DataContract]
+    public class InventoryItem
+    {
+        [DataMember(Name = "label")]
+        public string Label;
+
+        [DataMember(Name = "name")]
+        public string Name;
+
+        [DataMember(Name = "type")]
+        public string Type;
+
+        [DataMember(Name = "count")]
+        public int Count;
+
+        [DataMember(Name = "limit")]
+        public int Limit;
+
+        [DataMember(Name = "usable")]
+        public bool Usable;
+
+        [DataMember(Name = "canRemove")]
+        public bool IsRemovable;
+    }
+}
