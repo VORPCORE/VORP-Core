f554f9e0c3bb4218d6f6dd614d53da9943978e38
local9 <apjcookie1@hotmail.com>
2022-03-12 16:48:36 +0000

    Update time and weather methods

--- Vorp.Core.Client/RedM/World.cs
+++ Vorp.Core.Client/RedM/World.cs
@@ -4,7 +4,8 @@ namespace Vorp.Core.Client.RedM
 {
     internal class World
     {
-        public static void SetWeather(eWeatherType weatherType, bool transition = false, float transitionDuration = 0f) => Function.Call((Hash)0x2C6A07AF9AEDABD8, (long)weatherType, false, false, transition, transitionDuration, false);
+        public static void SetWeather(eWeatherType weatherType, bool transition = false, float transitionDuration = 0f) => Function.Call((Hash)0x2C6A07AF9AEDABD8, (long)weatherType, true, false, transition, transitionDuration, false);
+        public static void SetWeatherTransition(eWeatherType weatherType, eWeatherType weatherType2, float transition = 0f) => Function.Call((Hash)0xFA3E3CA8A1DE6D5D, (long)weatherType, (long)weatherType2, transition, true);
         public static void SetWeatherFrozen(bool frozen) => Function.Call((Hash)0xD74ACDF7DB8114AF, frozen);
 
         public static float WindSpeed
@@ -12,5 +13,8 @@ namespace Vorp.Core.Client.RedM
             get => Function.Call<float>((Hash)0xFFB7E74E041150A4);
             set => Function.Call((Hash)0xD00C2D82DC04A99F, value);
         }
+
+        public static void SetWindDirection(float direction) => Function.Call((Hash)0xB56C4F5F57A45600, direction);
+        public static Vector3 GetWindDirection => Function.Call<Vector3>((Hash)0xF703E82F3FE14A5F);
     }
 }
--- Vorp.Core.Client/RedM/WorldTime.cs
+++ Vorp.Core.Client/RedM/WorldTime.cs
@@ -14,12 +14,25 @@
                 PluginManager.Instance.AttachTickHandler(OnFreezeTime);
         }
 
+        public bool IsClockTimeOverridden => Function.Call<bool>((Hash)0xD7C95D322FF57522);
+        public void ClearClockTimeOverride() => Function.Call((Hash)0xD972DF67326F966E);
+
+        // NetworkClockTimeOverride_2
+        public void ClockTimeOverride(int hour, int minute, int transitionTime = 0, bool pauseClock = false, bool clockwise = false) => Function.Call((Hash)0xE28C13ECC36FF14E, hour, minute, 0, transitionTime, pauseClock, clockwise);
+
         public void Start() => PluginManager.Instance.AttachTickHandler(OnFreezeTime);
-        public void Stop() => PluginManager.Instance.DetachTickHandler(OnFreezeTime);
+
+        public void Stop()
+        {
+            if (IsClockTimeOverridden)
+                ClearClockTimeOverride();
+
+            PluginManager.Instance.DetachTickHandler(OnFreezeTime);
+        }
 
         private async Task OnFreezeTime()
         {
-            NetworkClockTimeOverride(Hour, Minute, 0, 0, true);
+            ClockTimeOverride(Hour, Minute, 0, true, true);
             SetClockTime(Hour, Minute, 0);
             PauseClock(true, 0);
         }
