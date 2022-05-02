1db2dc23b6d6a5782431314f789524e36337d303
local9 <apjcookie1@hotmail.com>
2022-05-02 19:51:37 +0100

    formatting

--- Vorp.Shared/Records/Character.cs
+++ Vorp.Shared/Records/Character.cs
@@ -51,55 +51,80 @@ namespace Vorp.Shared.Records
         #region Fields
         [Description("identifier")]
         public string SteamIdentifier { get; set; } = default!;
+
         [Description("steamname")]
         public string SteamName { get; set; } = default!;
+
         [Description("charidentifier")]
         public int CharacterId { get; set; }
+
         [Description("group")]
         public string Group { get; private set; } = "user";
+
         [Description("money")]
         public double Cash { get; private set; } = 0.00;
+
         [Description("gold")]
         public double Gold { get; private set; } = 0.00;
+
         [Description("rol")]
         public double RoleToken { get; private set; } = 0.00;
+
         [Description("xp")]
         public int Experience { get; private set; } = 0;
+
         // inventory should not be a string right now, it should be the output of a class
         [Description("inventory")]
         public string Inventory { get; set; } = "{}";
+
         [Description("job")]
         public string Job { get; private set; } = "unemployed";
+
         [Description("status")]
         public string Status { get; set; } = "{}";
+
         [Description("meta")]
         public string Meta { get; set; } = "{}";
+
         [Description("firstname")]
         public string Firstname { get; set; }
+
         [Description("lastname")]
         public string Lastname { get; set; }
+
         [Description("skinPlayer")]
         public string Skin { get; set; } = "{}";
+
         [Description("compPlayer")]
         public string Components { get; set; } = "{}";
+
         [Description("jobgrade")]
         public int JobGrade { get; set; } = 0;
+
         [Description("coords")]
         public string Coords { get; set; } = "{}";
+
         [Description("isdead")]
         public bool IsDead { get; set; } = false;
+
         [Description("clanid")]
         public int ClanId { get; set; } = 0;
+
         [Description("trust")]
         public int Trust { get; set; } = 0;
+
         [Description("supporter")]
         public int Supporter { get; set; } = 0;
+
         [Description("walk")]
         public string Walk { get; set; } = "noanim";
+
         [Description("crafting")]
         public string Crafting { get; set; } = "{}";
+
         [Description("info")]
         public string Info { get; set; } = "{}";
+
         [Description("gunsmith")]
         public double GunSmith { get; private set; } = 0.00;
 
