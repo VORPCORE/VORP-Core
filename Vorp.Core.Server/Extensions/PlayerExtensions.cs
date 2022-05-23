30a6216f1118442f00857f7530bfc818f5863044
local9 <apjcookie1@hotmail.com>
2022-05-23 17:18:39 +0100

    add ServerHandle method

--- Vorp.Core.Server/Extensions/PlayerExtensions.cs
+++ Vorp.Core.Server/Extensions/PlayerExtensions.cs
@@ -4,6 +4,8 @@ namespace Vorp.Core.Server.Extensions
 {
     static class PlayerExtensions
     {
+        public static int ServerHandle(this Player player) => int.Parse(player.Handle);
+
         public static User GetUser(this Player player)
         {
             return PluginManager.ToUser(player.Handle);
