310c65c59d1952de08b57e88c54a70bc46edfdf7
local9 <apjcookie1@hotmail.com>
2022-06-05 15:10:27 +0100

    Added CommandAliasAttribute

---
+++ Vorp.Shared/Attributes/CommandAliasAttribute.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Credit where credit is due, Xd_Golden_Tiger#0001 in FiveM C# Scripters Discord
+ * */
+
+namespace Vorp.Shared.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public class CommandAliasAttribute : Attribute
+    {
+        public string Description { get; }
+        public string[] Commands { get; }
+        public bool Restricted = false;
+
+        public CommandAliasAttribute(string discription, params string[] commands)
+        {
+            Description = discription;
+            Commands = commands;
+        }
+    }
+}
